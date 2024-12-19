66fcbfbf803943bbc0b61966f6ab70af086e1051
marinelegal <m.legall75@gmail.com>
2024-12-19 11:35:18 +0100

    d17 searching and else

--- day_16.py
+++ day_16.py
@@ -33,12 +33,17 @@ def count_columns(clean):
         count = 0
 
         for i in reversed(range(1,len(clean)-1)):
-            if pd.isna(clean.iloc[i-1, col]) and pd.isna(clean.iloc[i+1, col]) and clean.iloc[i, col] == '.':
+            # if pd.isna(clean.iloc[i-1, col]) and pd.isna(clean.iloc[i+1, col]) and clean.iloc[i, col] == '.':
+            #     not_poss.append(i)
+            #     # i+=2
+            #     # print('a')
+            #     continue
+            if pd.isna(clean.iloc[i-1, col]) and pd.isna(clean.iloc[i, col+1]) and pd.isna(clean.iloc[i, col-1]) and clean.iloc[i, col] == '.':
                 not_poss.append(i)
                 # i+=2
-                # print('a')
+                # print('b')
                 continue
-            elif pd.isna(clean.iloc[i-1, col]) and pd.isna(clean.iloc[i, col+1]) and pd.isna(clean.iloc[i, col-1]) and clean.iloc[i, col] == '.':
+            if pd.isna(clean.iloc[i+1, col]) and pd.isna(clean.iloc[i, col+1]) and pd.isna(clean.iloc[i, col-1]) and clean.iloc[i, col] == '.':
                 not_poss.append(i)
                 # i+=2
                 # print('b')
@@ -77,11 +82,11 @@ def count_rows(clean):
         count = 0
 
         for i in reversed(range(len(clean.columns)-1)):
-            if pd.isna(clean.iloc[row,i-1]) and pd.isna(clean.iloc[row,i+1]):
-                not_poss.append(i)
-                continue
+            # if pd.isna(clean.iloc[row,i-1]) and pd.isna(clean.iloc[row,i+1]):
+            #     not_poss.append(i)
+            #     continue
 
-            elif pd.isna(clean.iloc[row,i+1]) and pd.isna(clean.iloc[row-1,i]) and pd.isna(clean.iloc[row+1,i]):
+            if pd.isna(clean.iloc[row,i+1]) and pd.isna(clean.iloc[row-1,i]) and pd.isna(clean.iloc[row+1,i]):
                 not_poss.append(i)
                 continue
 
@@ -101,3 +106,38 @@ def count_rows(clean):
 
     max_row_score = np.array(list(counts_column.values())).sum()
     return max_row_score, traps
+
+
+def get_clean_rows_and_clean_cols(clean, traps_rows, traps_cols):
+    del_rows = []
+    for row in traps_rows:
+        for col in traps_rows[row]:
+            del_rows.append((row,col))
+
+    clean_rows = clean.copy()
+    for i in del_rows:
+        if clean_rows.iloc[i] == '.':
+            clean_rows.iloc[i]='X'
+
+
+    del_cols = []
+    for col in traps_cols:
+        for row in traps_cols[col]:
+            del_cols.append((row,col))
+
+    clean_cols = clean.copy()
+    for i in del_cols:
+        if clean_cols.iloc[i] == '.':
+            clean_cols.iloc[i]='X'
+
+    return clean_cols, clean_rows
+
+def merge_clean_rows_and_cols(clean, clean_rows, clean_cols):
+    clean_cop = clean.copy()
+    for i in range(len(clean_cop)):
+        for j in range(len(clean_cop.columns)):
+            if clean_cols.iloc[i,j] == '.' and clean_rows.iloc[i,j] == '.':
+                clean_cop.iloc[i,j] = '.'
+            elif clean_cols.iloc[i,j] == 'X' or clean_rows.iloc[i,j] == 'X':
+                clean_cop.iloc[i,j] = np.nan
+    return clean_cop
